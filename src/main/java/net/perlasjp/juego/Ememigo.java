da0aabe335d700513f058c0859be1c8db17c77b5
anahi veliz <anahiveliz262@gmail.com>
2025-07-30 16:41:30 -0600

    complete clases historia y enemigo

--- src/main/java/net/perlasjp/juego/Ememigo.java
+++ src/main/java/net/perlasjp/juego/Ememigo.java
@@ -1,5 +1,23 @@
 package net.perlasjp.juego;
 
 public class Ememigo {
+    private String nombre;
+    private int salud;
+    private int fuerza;
+
+    public int atacar() {
+        return 0;
+
+    }
+
+    public void recibirDanio(int danio) {
+
+    }
+
+    public boolean estaVivo() {
+
+        return false;
+
+    }
 
 }
