2231aa676df6ca5d070707ce606b6e9ad412cb83
oveth soriano <ovethsoriano2@gmail.com>
2025-08-05 08:26:56 -0600

    agregue ppersonaje completo juego

---
+++ src/main/java/net/perlasjp/juego/juego.java
@@ -0,0 +1,12 @@
+package net.perlasjp.juego;
+
+public class juego {
+    private Jugador jugador;
+    private Enemigo enemigo;
+    private Combate combate;
+
+    public void iniciar(){
+        
+    }
+
+}
