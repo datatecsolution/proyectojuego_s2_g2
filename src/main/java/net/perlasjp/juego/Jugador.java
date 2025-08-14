ea0607920ff3a965aa965fad5d22107c99956598
Sheyla Avalos <maribelavalos473@gmail.com>
2025-08-14 08:31:40 -0600

    agregue cambios a clase Jugador

--- src/main/java/net/perlasjp/juego/Jugador.java
+++ src/main/java/net/perlasjp/juego/Jugador.java
@@ -11,12 +11,33 @@ public int atacar() {
    return fuerza;
 
 }
-public void recibirDanios(int danio) {
+public void recibirDanios(int cantidad) {
 salud = salud - cantidad;
 if (salud < 0) salud = 0;
 }
 public boolean estaVivo() {
     return salud > 0;
+} 
+
+
+public Jugador(String n) {
+    nombre=n;
+    salud=100;
+    fuerza=10;
+
+
 }
 
+public String getNombre() {
+    return nombre;
+}
+
+public int getSalud() {
+    return salud;
+}
+public void mostrarEstado() {
+    System.out.println("Jugador:" + nombre);
+    System.out.println("Saludo" + salud);
+    System.out.println("Ataque" + fuerza);
+}
 }
