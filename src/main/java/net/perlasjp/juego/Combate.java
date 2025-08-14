e35ec95ee3136a57ea1c457815e4c50583af46ce
Anahi.Darwin <Amahi.Darwin>
2025-08-14 08:44:50 -0600

    agregue cambios a clase Combate

--- src/main/java/net/perlasjp/juego/Combate.java
+++ src/main/java/net/perlasjp/juego/Combate.java
@@ -1,10 +1,43 @@
 package net.perlasjp.juego;
 
+import java.util.Scanner;
+
 public class Combate {
     private Jugador jugador;
     private Enemigo Enemigo;
 
-    public void iniciar (Enemigo enemigo, Jugador jugador){
-        
-    }
+    public void iniciar (Jugador jugador,Enemigo enemigo) {
+        Scanner scanner = new scanner (System.in);
+    //System.out.println("\niUn" + enemigo.getNombre() + " ha aparecido!");
+
+    while (jugador.estaVivo() && enemigo.estaVivo()) {
+        System.out.println("\nTu salud: " + jugador.getSalud() + " | Salud
+    del enemigo: " + enemigo.getSalud());
+        System.out.print("¿Atacar (a) o Huir (h)?");
+        String opcion = scanner.nextLine();
+
+        if (opcion.equalsIgnoreCase("a")){
+            enemigo.recibirDanio(jugador.atacar());
+            if (enemigo.estaVivo()){
+                jugador.recibirDanio(enemigo.atacar());
+            } else {
+                System.out.println("¡Derrotaste al" + enemigo.getNombre() +
+                
+            }
+          }else if (opcion.equalsIgnoreCase("h")) {
+            System.out.println("Has huido del combate");
+            return;
+          } else {
+            System.out.println("Opción no válida");
+          }
+          }
+
+          if (!jugador.estaVivo()){
+            System.out.println("Has muerto en combate. Fin del juego.");
+            System.exit(0);
+          }
+        }
+
 }
+
+}}
