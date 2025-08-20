c02a55b0857c5a69c0accc3ef4b7e3867123e2b5
Erica Cruz <ericayanethcruz@gmail.com>
2025-08-20 10:41:23 -0600

    agregue cambios a clase historia

--- src/main/java/net/perlasjp/juego/Historia.java
+++ src/main/java/net/perlasjp/juego/Historia.java
@@ -3,6 +3,8 @@ package net.perlasjp.juego;
 public class Historia {
 
     public void Introduccion (){
+System.out.println ("\nEstás en un mundo lleno de peligro y misterio...");
+System.out.println("Tu mision: sobrevivir y descubrir los secretos del bosque");
 
     }
     
